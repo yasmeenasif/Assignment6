0307fc8924b17ce12f94593e48c10a530b240681
yasmeenasif <zaheeryasmeen05@gmail.com>
2025-09-29 17:38:07 +0530

    Added two files

---
+++ script.js
@@ -0,0 +1,17 @@
+let score=12;
+if(score<34)
+{
+   console.log("Fail-Grade E+") 
+}else if(score==35)
+{
+    console.log("Pass-Grade D+")
+}else if(score<60)
+{
+    console.log("Good-Grade C+")
+}else if(score<80)
+{
+    console.log("Exellent-Grade B+")
+}else if(score<=100)
+{
+    console.log("Outstanding-Grade A+")
+}
